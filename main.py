4ce21e5fd9d8167c1ea3375f7b9f7bd6cf701f3f
jaugustodantas <jnetodf@gmail.com>
2025-08-17 21:22:34 -0300

    começando a trabalhar a main

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main ():
     p = psicometria(temp,ur,pa)
     #print(p.razaoMistura())
     umidade0 = 20
+   #umidade1 = 16.67
     umidade1 = 16.67
     massaEspecifica = 750
     vazao = 300
@@ -18,13 +19,19 @@ def main ():
     vespeci = 0.96
     tempM = 40
     h = 0.05
+    incremento = 1 /h
     cs = calculoSecagem(umidade0,massaEspecifica,vazao,area,tempo,w,tempSec,tempM,pa,umidade1,vespeci,h)
-    wf = cs.calculoRazaoMisturaAr()
-    tempSec = cs.calculoTemperaturaFinais()
-    cs.to = tempSec
-    cs.w = wf
-    p1 = cs.calculoUmidadeRelativaSaida()
-    print(p1)
+    for x in range(int(incremento)):
+        wf = cs.calculoRazaoMisturaAr()
+        tempSec = cs.calculoTemperaturaFinais()
+        umidade3 = cs.calculoUmidadeAposIncremeto()
+        cs.to = tempSec
+        cs.w = wf
+        p1 = cs.calculoUmidadeRelativaSaida()
+        print(f"A umidade da massa na altura {h:.2f} é de {(umidade3 * 100):.2f} e a temperatura ficou em {tempSec:.2f} e a umidade relativa de saida foi {p1:.2f}")
+        h=h+0.05
+        #cs.h=h
+
 
 if __name__ == "__main__":
     main()
